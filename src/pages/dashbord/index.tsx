5a5ce5e3f4c4882a48a32a60b5cdb29971b2ad0c
Lucas Fernando <73972922+DigitalHDR@users.noreply.github.com>
2023-09-21 13:33:49 -0300

    Cadastrando no banco de dados

--- src/pages/dashbord/index.tsx
+++ src/pages/dashbord/index.tsx
@@ -8,7 +8,17 @@ import { Textarea } from '../../components/textarea'
 import { FiShare2 } from 'react-icons/fi'
 import { FaTrash } from 'react-icons/fa'
 
-export default function Dashboard() {
+import { db } from '../../services/firebaseConection'
+
+import { addDoc, collection } from 'firebase/firestore'
+
+interface HomeProps {
+  user: {
+    email: string
+  }
+}
+
+export default function Dashboard({ user }: HomeProps) {
   const [input, setInput] = useState('')
   const [publicTask, setPublicTask] = useState(false)
 
@@ -16,12 +26,24 @@ export default function Dashboard() {
     setPublicTask(event.target.checked)
   }
 
-  function handleRegisterTask(event: FormEvent) {
+  async function handleRegisterTask(event: FormEvent) {
     event.preventDefault()
 
-    if(input === '') return
+    if (input === '') return
 
-    alert('teste')
+    try {
+      await addDoc(collection(db, 'tarefas'), {
+        tarefa: input,
+        created: new Date(),
+        user: user?.email,
+        public: publicTask,
+      })
+
+      setInput('')
+      setPublicTask(false)
+    } catch (err) {
+      console.log(err)
+    }
   }
 
   return (
@@ -99,6 +121,10 @@ export const getServerSideProps: GetServerSideProps = async ({ req }) => {
   }
 
   return {
-    props: {},
+    props: {
+      user: {
+        email: session?.user?.email,
+      },
+    },
   }
 }
